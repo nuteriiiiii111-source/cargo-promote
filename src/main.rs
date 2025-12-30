ffbea244850da014946422c1365b94936b51818d
Norbert Miszta <norbert@host144.private.net>
2025-12-30 21:02:00 +0100

    fix: start args at 1

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ mod x;
 use crate::x::*;
 
 fn main() {
-    let mut args = env::args();
-    let _ = args.next();
+    let mut args = env::args().skip(1);
     let mut path = args.next();
     if path == Some("-d".to_string()) {
         path = args.next();
